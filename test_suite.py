259944a70dfc9a1d0d32a2787e69653e003d6466
Sophie Chen <boziying@gmail.com>
2022-12-23 21:02:05 -0800

    unittest

--- test_suite.py
+++ test_suite.py
@@ -1,16 +1,16 @@
 import unittest
-import test_User
-import test_calculation
-import test_bankingUI
-import test_bankingDS
+from test_User import *
+from test_calculation import *
+from test_bankingUI import *
+from test_bankingDS import *
 
 
 def my_suite():
     suite = unittest.TestSuite()
     result = unittest.TestResult()
-    #suite.addTest(unittest.makeSuite(test_User))
+    suite.addTest(unittest.makeSuite(test_User))
     suite.addTest(unittest.makeSuite(test_calculation))
-    suite.addTest(unittest.makeSuite(test_bankingUI))
+    suite.addTest(unittest.makeSuite( test_bankingUI))
     suite.addTest(unittest.makeSuite(test_bankingDS))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
